0ac4f3da6ef89c9cea998e7b93aad45d299173b6
José Ricardo A. Figueirôa <jraf@cin.ufpe.br>
2020-11-26 09:52:09 -0300

    circuit test added

--- tests.py
+++ tests.py
@@ -1,12 +1,23 @@
 import unittest
 import numpy as np
+from encoding import Encoding
+from main import gen_angles, gen_circuit
 
-from main import gen_angles
+from qiskit import Aer, execute, QuantumCircuit
 
 state_default = list(map(lambda a: np.sqrt(a), [.03, .07, .15, .05, .1, .3, .2, .1]))
+
+def normalize(v):
+    return v / np.linalg.norm(v)
 
 class test(unittest.TestCase):
     def test1(self):
         assert(np.allclose(gen_angles(state_default), [1.98, 1.91, 1.43, 1.98, 1.05, 2.09, 1.23], rtol=.01))
     def test2(self):
-        return
+        backend = Aer.get_backend('statevector_simulator')
+        get_result = lambda circuit: execute(circuit, backend).result().get_statevector()
+
+        r1 = get_result( Encoding(np.array(state_default), 'dc_amplitude_encoding').qcircuit )
+        r2 = get_result( gen_circuit( gen_angles(state_default) ) )
+
+        assert(np.allclose(r2, r1, rtol=0.01))
